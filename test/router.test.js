40c3abd5918a0209bf0fcc2e52a8a68c56fe0b6a
Nicolas Hery <hery.nicolas@gmail.com>
2014-07-24 16:34:48 -0400

    Finish tests

--- test/router.test.js
+++ test/router.test.js
@@ -70,4 +70,115 @@ describe('router', function() {
       expect(handler).to.have.been.calledWith('/foo', null);
     });
   });
+
+  describe('updateBrowserUri', function() {
+    var router, browser;
+
+    beforeEach(function() {
+      browser = {
+        getCurrentUri: sinon.spy(),
+        replaceUri: sinon.spy(),
+        setUri: sinon.spy()
+      };
+      router = proxyquire('../router', {
+        './lib/browser': browser
+      });
+    });
+
+    it('updates browser with correct uri from route object', function() {
+      var route = {
+        path: '/item/:id',
+        params: {id: '123'}
+      };
+
+      router.updateBrowserUri(route);
+
+      expect(browser.setUri).to.have.been.calledWith('/item/123');
+    });
+
+    it('can replace uri without adding entry in browser history', function() {
+      var route = {
+        path: '/item/:id',
+        params: {id: '123'}
+      };
+
+      router.updateBrowserUri(route, {replace: true});
+
+      expect(browser.replaceUri).to.have.been.calledWith('/item/123');
+    });
+
+    it('does not trigger uri change handler', function() {
+      var handler = sinon.spy();
+      router.setOnChangeHandler(handler);
+      var route = {
+        path: '/item/:id',
+        params: {id: '123'}
+      };
+
+      router.updateBrowserUri(route);
+
+      expect(handler).to.not.have.been.called;
+    });
+
+    it('does not do anything if new uri is same as current uri', function() {
+      browser.getCurrentUri = sinon.stub().returns('/item/123');
+      var route = {
+        path: '/item/:id',
+        params: {id: '123'}
+      };
+
+      router.updateBrowserUri(route);
+
+      expect(browser.setUri).to.not.have.been.called;
+      expect(browser.replaceUri).to.not.have.been.called;
+    });
+  });
+
+  describe('isRouteMatched', function() {
+    var router;
+
+    beforeEach(function() {
+      router = proxyquire('../router', {});
+      router.setMatchedPaths(['/item/:id']);
+    });
+
+    it('returns true if route is matched', function() {
+      var match = router.isRouteMatched({
+        path: '/item/:id',
+        params: {id: '123'}
+      });
+
+      expect(match).to.be.true;
+    });
+
+    it('returns false if route is not matched', function() {
+      var match = router.isRouteMatched({
+        path: '/foo'
+      });
+
+      expect(match).to.be.false;
+    });
+
+    it('returns false if no route given', function() {
+      var match = router.isRouteMatched(null);
+
+      expect(match).to.be.false;
+    });
+  });
+
+  describe('uriFromRoute', function() {
+    var Route = {
+      toUri: sinon.spy()
+    };
+    var router = proxyquire('../router', {
+      './lib/Route': Route
+    });
+
+    it('calls Route.toUri', function() {
+      var route = {path: '/items'};
+      router.uriFromRoute(route);
+
+      expect(Route.toUri).to.have.been.calledWith(sinon.match(route));
+    });
+  });
 });
